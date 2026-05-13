92d94b3a0117b72f9ef26ffdc1d7db3a7ccd34d6
jorgeuriel89-rgb <jorgeuriel89@gmail.com>
2026-05-12 23:26:48 -0600

    Se grega migracion y modelos con sus relaciones

---
+++ app/Models/Pedido.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['cliente_id', 'total', 'estado', 'fecha_entrega'];
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function productos()
+    {
+        return $this->belongsToMany(Producto::class, 'pedido_producto');
+    }
+
+    // Local Scopes
+    public function scopePorEnviar($query)
+    {
+        return $query->where('estado', 'pendiente')
+                     ->whereBetween('fecha_entrega', [now(), now()->addDays(3)]);
+    }
+
+    public function scopeRetrasados($query)
+    {
+        return $query->where('estado', 'pendiente')
+                     ->where('fecha_entrega', '<', now()->startOfDay());
+    }
+
+    public function scopeEntregados($query)
+    {
+        return $query->where('estado', 'entregado');
+    }
+
+    public function scopeCancelados($query)
+    {
+        return $query->where('estado', 'cancelado');
+    }
+}
