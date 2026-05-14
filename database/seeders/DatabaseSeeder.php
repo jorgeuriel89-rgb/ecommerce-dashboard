8702c9da3d69c9a5e200a825ffa5bd94ec88ad8b
jorgeuriel89-rgb <jorgeuriel89@gmail.com>
2026-05-14 09:29:57 -0600

    actualizacion AI_JOURNEY

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,14 +18,20 @@ class DatabaseSeeder extends Seeder
         Cliente::factory(100)->create();
 
         $productos = Producto::all();
-        $clientes = Cliente::all();
+        $clientes = Cliente::pluck('id');
 
         // Crear 1000 pedidos asignados a clientes existentes
-        Pedido::factory(1000)->make()->each(function ($pedido) use ($productos, $clientes) {
-            $pedido->cliente_id = $clientes->random()->id;
-            $pedido->save();
-            $randomProductos = $productos->random(rand(1, 5))->pluck('id');
-            $pedido->productos()->attach($randomProductos);
-        });
+        for ($i = 0; $i < 1000; $i++) {
+            $pedido = Pedido::create([
+                'cliente_id' => $clientes->random(),
+                'total' => fake()->randomFloat(2, 100, 5000),
+                'estado' => fake()->randomElement(['pendiente', 'entregado', 'cancelado']),
+                'fecha_entrega' => fake()->dateTimeBetween('-30 days', '+30 days')->format('Y-m-d'),
+            ]);
+
+            $pedido->productos()->attach(
+                $productos->random(rand(1, 5))->pluck('id')
+            );
+        }
     }
 }
