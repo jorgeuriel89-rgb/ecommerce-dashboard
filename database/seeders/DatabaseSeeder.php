c2874bdd42a27d02d8dadff8a625964c89034339
jorgeuriel89-rgb <jorgeuriel89@gmail.com>
2026-05-14 09:15:43 -0600

    ajuste en los datos generados, evita repetidos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,16 @@ class DatabaseSeeder extends Seeder
         // Crear 20 productos
         Producto::factory(20)->create();
 
-        // Crear 50 clientes
-        Cliente::factory(50)->create();
+        // Crear 100 clientes
+        Cliente::factory(100)->create();
 
         $productos = Producto::all();
+        $clientes = Cliente::all();
 
-        // Crear 1000 pedidos
-        Pedido::factory(1000)->create()->each(function ($pedido) use ($productos) {
+        // Crear 1000 pedidos asignados a clientes existentes
+        Pedido::factory(1000)->make()->each(function ($pedido) use ($productos, $clientes) {
+            $pedido->cliente_id = $clientes->random()->id;
+            $pedido->save();
             $randomProductos = $productos->random(rand(1, 5))->pluck('id');
             $pedido->productos()->attach($randomProductos);
         });
